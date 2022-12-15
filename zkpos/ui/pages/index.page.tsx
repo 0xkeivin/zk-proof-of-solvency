21b0de9396a2937d0deec4a14ca8b573b90f0ebb
0xkeivin <keivincatwork@gmail.com>
2022-12-16 03:30:26 +0800

    removed commented chunk

--- zkpos/ui/pages/index.page.tsx
+++ zkpos/ui/pages/index.page.tsx
@@ -57,39 +57,7 @@ export default function Home() {
   // My deployed contract: B62qrRt1HpXupeJJef3GkRXKAoZi2iiajzJjxXJGtp8qqeNoQNm6g8Q
   const zkAppAddress =
     "B62qisn669bZqsh8yMWkNyCA7RvjrL6gfdr3TQxymDHNhTc97xE5kNV";
-  // create useEffect hook to run code on page load
-  // useEffect(() => {
-  //   // create async function to run code
-  //   (async () => {
-  //     // wait for snarkyjs to be ready
-  //     const isSnarkyReady = await isReady;
-  //     log.debug(`isSnarkyReady: ${isSnarkyReady}`);
-  //     setZkAppPublicKey( PublicKey.fromBase58(zkAppAddress));
-  //     const graphqlEndpoint = "https://proxy.berkeley.minaexplorer.com/graphql";
-  //     setGraphqlEndpoint(graphqlEndpoint);
-  //     let Berkeley = minaNetwork.current.Network(graphqlEndpoint);
-  //     const setNetworkRes = minaNetwork.current.setActiveInstance(Berkeley);
-  //     log.info(`setNetworkRes: ${setNetworkRes}`);
-  //     // load the contract
-  //     const { BasicMerkleTreeContract } = await import(
-  //       "../../contracts/build/src/"
-  //     );
-  //     const { Add } = await import("../../contracts/build/src/");
-
-  //     // This should be removed once the zkAppAddress is updated.
-  //     if (!zkAppAddress) {
-  //       log.error(
-  //         'The following error is caused because the zkAppAddress has an empty string as the public key. Update the zkAppAddress with the public key for your zkApp account, or try this address for an example "Add" smart contract that we deployed to Berkeley Testnet: B62qqkb7hD1We6gEfrcqosKt9C398VLp1WXeTo1i9boPoqF7B1LxHg4'
-  //       );
-  //     }
-  //     // log.info(`zkApp JSON: ${JSON.stringify(account)}`);
-  //     // log.info(`zkApp state: ${JSON.stringify(account?.appState)}`);
-
-  //     // log.info(`error: ${error}`);
-  //     // await Add.compile();
-  //   })();
-  // }, []);
-
+ 
   // --- copied from tutorial
   useEffect(() => {
     (async () => {
