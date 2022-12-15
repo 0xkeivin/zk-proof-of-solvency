a064d3faee123049af6a4d658d883eca1079d6b5
0xkeivin <keivincatwork@gmail.com>
2022-12-15 17:34:59 +0800

    added more utils and tidied up

--- zkpos/ui/pages/index.page.tsx
+++ zkpos/ui/pages/index.page.tsx
@@ -1,7 +1,4 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import type { BasicMerkleTreeContract } from "../../contracts/src/";
 import {
   Mina,
@@ -13,31 +10,32 @@ import {
 import log from "loglevel";
 import ZkappWorkerClient from "./zkappWorkerClient";
 import {
-  Button,
   Card,
   ChakraProvider,
   CardBody,
-  Divider,
   HStack,
   Heading,
   Link,
   Stack,
   Spacer,
   Text,
-  VStack,
   CardHeader,
 } from "@chakra-ui/react";
 import { ExternalLinkIcon } from "@chakra-ui/icons";
 import StateCard from "../components/StateCard";
+import { shortenAddress } from "../utils/shortenAddress";
+import { updateAddContract } from "../utils/updateAddContract";
 // set log level
 log.setLevel("debug");
 export default function Home() {
   const [accountState, setAccountState] = useState<String | undefined>("");
+  const minaNetwork = useRef(Mina);
+
   // Fetch the account from Berkeley Testnet
   // Sample contract: B62qisn669bZqsh8yMWkNyCA7RvjrL6gfdr3TQxymDHNhTc97xE5kNV
   // My deployed contract: B62qrRt1HpXupeJJef3GkRXKAoZi2iiajzJjxXJGtp8qqeNoQNm6g8Q
   const zkAppAddress =
-    "B62qrRt1HpXupeJJef3GkRXKAoZi2iiajzJjxXJGtp8qqeNoQNm6g8Q";
+    "B62qisn669bZqsh8yMWkNyCA7RvjrL6gfdr3TQxymDHNhTc97xE5kNV";
   // create useEffect hook to run code on page load
   useEffect(() => {
     // create async function to run code
@@ -47,8 +45,8 @@ export default function Home() {
       log.debug(`isSnarkyReady: ${isSnarkyReady}`);
       const graphqlEndpoint = "https://proxy.berkeley.minaexplorer.com/graphql";
       setGraphqlEndpoint(graphqlEndpoint);
-      let Berkeley = Mina.Network(graphqlEndpoint);
-      const setNetworkRes = Mina.setActiveInstance(Berkeley);
+      let Berkeley = minaNetwork.current.Network(graphqlEndpoint);
+      const setNetworkRes = minaNetwork.current.setActiveInstance(Berkeley);
       log.info(`setNetworkRes: ${setNetworkRes}`);
       // load the contract
       const { BasicMerkleTreeContract } = await import(
@@ -85,11 +83,6 @@ export default function Home() {
   const setStateHandler = async () => {
     log.info("setStateHandler: Clicked");
   };
-  // create a function that shortens the public key
-  const shortenAddress = (address: string) => {
-    return address.slice(0, 5) + "..." + address.slice(-5);
-  };
-
   return (
     <>
       <ChakraProvider>
@@ -104,7 +97,7 @@ export default function Home() {
             <Card width="md" bg="gray.200">
               <CardBody>
                 <HStack>
-                  <div>zkApp Address: </div>
+                  <Text as="b">zkApp Address: </Text>
                   <Link
                     href={
                       "https://berkeley.minaexplorer.com/wallet/" + zkAppAddress
